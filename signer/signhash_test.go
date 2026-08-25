7385651f9fcb3c965f504d2d6ee5d2c6b26f8860
Lee <7932644+strahe@users.noreply.github.com>
2026-08-25 13:58:14 +0800

    style(signer): satisfy gofumpt

--- signer/signhash_test.go
+++ signer/signhash_test.go
@@ -63,6 +63,7 @@ type wrappedEVMSigner struct {
 func (w *wrappedEVMSigner) FilecoinAddress() address.Address           { return w.inner.FilecoinAddress() }
 func (w *wrappedEVMSigner) Sign(msg []byte) (*crypto.Signature, error) { return w.inner.Sign(msg) }
 func (w *wrappedEVMSigner) EVMAddress() common.Address                 { return w.inner.EVMAddress() }
+
 func (w *wrappedEVMSigner) Transactor(chainID *big.Int) (*bind.TransactOpts, error) {
 	return w.inner.Transactor(chainID)
 }
